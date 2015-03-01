5c6faf7d626ff8f26f2a98fa42d7b9aa27aaf6a9
Andrew Betts <andrew.betts@ft.com>
2015-03-01 11:36:01 +0000

    Finish initial build

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 var noble = require('noble');
-var request = require('request');
+var request = require('request-promise');
+var bunyan = require('bunyan');
+var bunyanLogentries = require('bunyan-logentries');
+var moment = require('moment');
 
-var senseiurl = process.env.TRIBOT_URL || 'http://sensei.origami.trib.tv';
-var location = process.env.LOCATION_DESC || 'Andrew\'s flat';
+var uribeaconUUID = 'fed8';
+var location = process.env.LOCATION_DESC || 'Unknown';
+var updateInterval = 30 * 60;
 
+// Set up logging.  If a Logentries token has been provided, log to hosted service
+var logStreams = [{stream: process.stdout, level: 'trace'}];
+if (process.env.LOGENTRIES_TOKEN) {
+	logStreams.push({
+		stream: bunyanLogentries.createStream({token: process.env.LOGENTRIES_TOKEN}),
+		level: 'info',
+		type:'raw'
+	});
+}
+var log = bunyan.createLogger({
+	name: 'uribeacon-checkin',
+	streams: logStreams
+});
+
+var beacons = {};
+
+
+// Expansion strings (spec: https://github.com/google/uribeacon/blob/master/specification/AdvertisingMode.md)
 var uriprefixes = {
 	0: 'http://www.',
 	1: 'https://www.',
@@ -28,35 +50,65 @@ var expansions = {
 	13: '.gov'
 }
 
-function announce(name) {
-	request({
-		url: senseiurl+"/rooms/bot-playground",
-		method: 'POST',
-		headers: {'Content-Type': 'text/plain'},
-		body: name + ' has arrived in ' + location
-	});
-}
-
 noble.on('stateChange', function(state) {
 	if (state === 'poweredOn') {
-		console.log('Starting scan');
-		noble.startScanning(['fed8'], true);
+		log.info('Starting scan', {location:location});
+		noble.startScanning([uribeaconUUID], true);
 	}
 });
 
 noble.on('discover', function(peripheral) {
 	var rssi = peripheral.rssi;
 
+	log.trace('Discovered peripheral', peripheral);
+
 	peripheral.advertisement.serviceData.forEach(function(record) {
-		if (record.uuid === 'fed8') {
+		if (record.uuid === uribeaconUUID) {
 			var b = new Buffer(record.data);
 			var flags = b.readUInt8(0);
 			var transmitpower = b.readInt8(1);
-			var url = uriprefixes[b.readUInt8(2)];
+			var beaconurl = uriprefixes[b.readUInt8(2)];
 			for (var i=3, s=b.length; i<s; i++) {
-				url += b.readUInt8(i) in expansions ? expansions[b.readUInt8(i)] : b.toString('ascii', i, i+1);
+				beaconurl += b.readUInt8(i) in expansions ? expansions[b.readUInt8(i)] : b.toString('ascii', i, i+1);
 			}
-			console.log(record.data.toString('ascii'), url, rssi);
+
+			log.debug('Found URIBeacon', {raw: b, beaconurl: beaconurl, rssi: rssi});
+
+			if (beaconurl in beacons && moment(beacons[beaconurl].lastSeenDate).isAfter(moment().subtract(updateInterval, 's'))) {
+				log.debug('Update not yet due', {beaconurl:beaconurl, data:beacons[beaconurl]});
+			} else {
+
+				// Follow url until it returns something other than a redirect
+				var url = beaconurl;
+				request({
+					url: url,
+					followRedirect: function(r) {
+						log.trace('Redirect', {from:url, to:r.headers.location});
+						url = r.headers.location;
+						return true;
+					}
+				}).then(function(data) {
+					data = JSON.parse(data);
+					log.debug("Loaded beacon data", data);
+					data.lastSeenLocation = location;
+					data.lastSeenDate = (new Date()).toISOString();
+					return request({
+						url: url,
+						method: 'PUT',
+						json: true,
+						body: data
+					}).then(function(resp) {
+						if (resp.ok) {
+							log.info('Spotted', {name:data.displayName, username:data.userName, location:location, url:url, beaconurl:beaconurl});
+							beacons[beaconurl] = data;
+						} else {
+							throw new Error(resp);
+						}
+					});
+				}).catch(function(e) {
+					log.warn(e);
+				});
+			}
 		}
 	});
 });
